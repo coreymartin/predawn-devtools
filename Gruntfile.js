1a9793017055b1f7a060cbec282fbd55fd905134
Corey <coreyn.martin@gmail.com>
2015-01-25 00:33:07 -0600

    Initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,45 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    sass: {
+      dist: {
+        options: {
+          compass:true,
+          style:'compressed',
+          cacheLocation: '/Users/Corey/.sass-cache',
+          trace:false
+        },
+        files: {
+          'corey-style.css' : 'corey-style.scss'
+        }
+      }
+    },
+    watch: {
+      sass: {
+        files: ['*.scss'],
+        tasks: ['sass', 'notify:sass']
+      }
+    },
+    notify: {
+      sass: {
+        options: {
+          title: 'Task Complete',  // optional
+          message: 'SASS compiled successfully.', //required
+        }
+      }
+    }
+  });
+
+  grunt.registerTask('loadSitemap', function() {
+    var sitemap_urls = grunt.file.readJSON('./sitemap.json');
+    grunt.config.set('uncss.dist.options.urls', sitemap_urls);
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-notify');
+  grunt.registerTask('default',['sass', 'watch']);
+
+
+};
